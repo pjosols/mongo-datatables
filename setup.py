5abc1461f9438ae5efb08e160fcaf76f6a907f38
Paul Olsen <paul@wholeshoot.com>
2026-03-15 09:15:55 -0400

    v1.27.3: Remove unused sum/avg accumulators from _get_rowgroup_data

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except FileNotFoundError:
 
 setup(
     name='mongo_datatables',
-    version='1.28.0',
+    version='1.27.3',
     description='Server-side processing for DataTables and Editor with MongoDB',
     long_description=long_description,
     long_description_content_type=long_description_content_type,
