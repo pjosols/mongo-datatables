cadbc1f349f6c7dce44a0e6660da4c09ec0b61a8
Paul Olsen <paul@wholeshoot.com>
2026-03-15 14:29:44 -0400

    v1.36.1 — Quality: narrow except Exception in query_builder.py

--- mongo_datatables/__init__.py
+++ mongo_datatables/__init__.py
@@ -1,7 +1,7 @@
 """
 MongoDB integration with jQuery DataTables
 """
-__version__ = '1.35.0'
+__version__ = '1.36.1'
 
 from mongo_datatables.datatables import DataTables, DataField
 from mongo_datatables.editor import Editor
