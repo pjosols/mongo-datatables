bbb1a4fccfdfab3c1551e572d97e4cda2c2081b9
Paul Olsen <paul@wholeshoot.com>
2026-03-15 12:19:24 -0400

    v1.30.0: structural fixes, _hashable outside loop, _sb_date between semantics

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except FileNotFoundError:
 
 setup(
     name='mongo_datatables',
-    version='1.29.6',
+    version='1.30.0',
     description='Server-side processing for DataTables and Editor with MongoDB',
     long_description=long_description,
     long_description_content_type=long_description_content_type,
